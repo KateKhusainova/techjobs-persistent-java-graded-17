7be5590887e0888994fd6a92c1411e7e02030709
Ekaterina <katekhusainova8@gmail.com>
2023-11-10 12:23:26 -0500

    Finished GA4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,18 @@
 --Part 1
+id int PK
+employer varchar(255)
+name varchar(255)
+skills varchar(255)
 
 --Part 2
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 --Part 3
+DROP TABLE job;
 
 --Part 4
+SELECT *
+FROM skill
+INNER JOIN job_skills ON job_skills.skills_id = skill.id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,33 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Location is required")
+    @Size(min = 3, max = 255)
+    private String location;
+
+    @OneToMany
+    @JoinColumn (name="employer_id")
+    private final List<Job> jobs = new ArrayList<>();
+
+    public Employer () {}
+
+    public Employer(String location) {
+        super();
+        this.location = location;
+    }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+
 }
